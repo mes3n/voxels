7f4011762bd9dd4b06003fabdfe5434082ceede5
mes3n <skidmackan@gmail.com>
2022-07-03 02:43:59 +0200

    added class for creating of one cube

---
+++ src/cube.cpp
@@ -0,0 +1,20 @@
+#include "cube.hpp"
+#include "cubeVertices.hpp"
+
+Cube::Cube () {
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(GL_FLOAT), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5*sizeof(GL_FLOAT), (void*)(3*sizeof(GL_FLOAT)));
+    glEnableVertexAttribArray(1);
+
+}
+
+void Cube::use (void) const {
+    glBindVertexArray(vao);
+}
