c7293123cdf46d2851cb4fbcad7a4c67c8b0d26e
mes3n <skidmackan@gmail.com>
2022-07-11 03:57:43 +0200

    added class for blocks to be included in chunk, atm

---
+++ src/world/blocks/cube.cpp
@@ -0,0 +1,30 @@
+#include "cube.hpp"
+#include "cubeVertices.hpp"
+
+Cube::Cube () {
+    GLuint vbo;
+
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(GL_FLOAT), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(GL_FLOAT), (void*)(3*sizeof(GL_FLOAT)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+
+}
+
+void Cube::use (void) const {
+    glBindVertexArray(vao);
+}
+
+GLuint Cube::getVAO (void) const {
+    return vao;
+}
